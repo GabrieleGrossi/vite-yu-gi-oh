dbd8b7235e5a40c937a8c882c8f8228d716170a7
GabrieleGrossi <grossigabriele98@gmail.com>
2023-06-19 17:28:08 +0200

    add axios and create the cards

--- src/components/AppCardList.vue
+++ src/components/AppCardList.vue
@@ -1,20 +1,40 @@
 <template>
-    <div>
-        <h2>
-            App Card List
-        </h2>
-        <AppSingleCard/>
+    <div class="container">
+        <div class="row">
+            <div class="col-2">
+                <AppSingleCard v-for="card in cardList"
+                    :cardName="card.name"
+                    :cardType="card.type"
+                    :cardImage="card.card_images"
+                />
+            </div>
+        </div>
     </div>
 </template>
 <script>
 import AppSingleCard from './AppSingleCard.vue';
+import axios from 'axios';
 export default {
     name:'AppCardList',
+    data(){
+        return{
+            cardList : [],
+        }
+    },
     components:{
         AppSingleCard
     },
     created(){
-
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+        .then( (response) => {
+            // handle success
+            console.log(response.data.results);
+            this.cardList = response.data.results;
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        });
     }
 }
 </script>
--- src/components/AppSingleCard.vue
+++ src/components/AppSingleCard.vue
@@ -1,13 +1,31 @@
 <template>
     <div>
-        <h2>
-            App Single Card
-        </h2>
+        <div class="card" style="width: 18rem;">
+            <img :src="card_images" class="" alt='name + "image"'>
+            <div class="card-body">
+                <h4 class="card-title">
+                    {{ name }}
+                </h4>
+                <h4 class="card-text">
+                    {{ type }}
+                </h4>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 export default {
-    name:'AppSingleCard'
+    name:'AppSingleCard',
+    data(){
+        return{
+
+        }
+    },
+    props:{
+        name: String,
+        type: String,
+        card_images: String
+    }
 }
 </script>
 <style lang="scss" scoped>
