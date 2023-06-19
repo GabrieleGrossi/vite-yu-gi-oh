4ba81df1adf1b4a05117a95fe728329f07d79c5f
GabrieleGrossi <grossigabriele98@gmail.com>
2023-06-19 16:09:13 +0200

    Import style in header and add title whit pic

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,9 @@
 <template>
     <header>
-        <h1>App Header</h1>    
+        <img src="../assets/img/MtG_logo.jpg" alt="Correct logo">  
+        <h1>
+            Yu-Gi-Oh Api is for children
+        </h1>  
     </header>
 </template>
 <script>
@@ -9,5 +12,20 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-    
+    @use '../style/general' as *;
+    @use '../style/partials/variables' as *;
+    @use '../style/partials/mixins' as *;
+
+    header {
+        @include flex(row, start, center, no-wrap);
+        padding: 1rem;
+
+        img {
+            width: 250px;
+        };
+
+        h1 {
+            margin-left: 1.5rem;
+        }
+    }
 </style>
