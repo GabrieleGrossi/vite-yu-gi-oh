4e62e4d8524b9afcbf810ca64487c23a779e199f
GabrieleGrossi <grossigabriele98@gmail.com>
2023-06-19 19:49:17 +0200

    not exactly the same exercise but try

--- src/components/AppCardList.vue
+++ src/components/AppCardList.vue
@@ -1,15 +1,26 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-2">
-                <AppSingleCard v-for="card in cardList"
-                    :name="card.name"
-                    :type="card.type"
-                    :image="card.card_images[0].image_url"
-                />
+    <section>
+        <div class="my-container black">
+            <div class="my-row">
+                <div class="col-12">
+                    <h2 class="my-search">
+                        You found {{ cardList.length }} cards
+                    </h2>
+                </div>
             </div>
         </div>
-    </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex flex-wrap ">
+                    <AppSingleCard v-for="card in cardList"
+                        :name="card.name"
+                        :type="card.type"
+                        :image="card.card_images[0].image_url"
+                    />
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 <script>
 import AppSingleCard from './AppSingleCard.vue';
@@ -41,11 +52,25 @@ export default {
 </script>
 <style lang="scss" scoped>
 @use '../style/partials/mixins';
-    *{
+    section{
         background-color: white;
+        width: 80%;
+        margin: 0 auto;
     }
-    .row{
-        display: flex;
-        flex-direction: row;
+    .my-container{
+        width: 80%;
+        margin: 0 auto;
+    }
+    .my-row{
+        width: 50%;
+        margin-toptop: 2rem;
+    }
+    .my-search{
+        padding-top: 2rem;
+    }
+    .black{
+        background-color: black;
+        color: white;
+        width: calc(100% - 6rem);
     }
 </style>
--- src/components/AppSingleCard.vue
+++ src/components/AppSingleCard.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
-        <div class="card" style="width: 18rem;">
+    <div class="container-image">
+        <div class="card">
             <img :src="image" class="card-img-top" :alt='name + "image"'>
             <div class="card-body">
                 <h4 class="card-title">
-                    {{ name }}
+                        {{ name }}
                 </h4>
                 <h5 class="card-text">
-                    {{ type }}
+                        {{ type }}
                 </h5>
             </div>
         </div>
@@ -29,5 +29,13 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-    
+    .card {
+    height: 100%;
+    width: 150px;
+    margin-left: 2.7rem;
+    background-color: #d48f38;
+    }
+    .container-image{
+        margin-bottom: 2rem;
+    }
 </style>
